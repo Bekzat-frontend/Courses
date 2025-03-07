6dc8e410086a3f667c931f6da8342e0fa1aee92c
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-08 02:00:11 +0700

    kjadc

--- src/pages/AppRouter.jsx
+++ src/pages/AppRouter.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import KursPage from "./KursPage";
-import Raspisanya from "./Raspisanya";
-import MainLayout from "./MainLayout";
+// import React from "react";
+// import { Route, Routes, Navigate } from "react-router-dom";
+// import KursPage from "./KursPage";
+// import Raspisanya from "./Raspisanya";
+// import MainLayout from "./MainLayout";
 
-function AppRouter() {
-  return (
-    <Routes>
-      <Route path="/*" element={<MainLayout />}>
-        <Route index element={<Navigate to={<KursPage />} />} />
-        <Route path="Courses" element={<KursPage />} />
+// function AppRouter() {
+//   return (
+//     <Routes>
+//       <Route path="/*" element={<MainLayout />}>
+//         <Route index element={<Navigate to={<KursPage />} />} />
+//         <Route path="Courses" element={<KursPage />} />
 
-        <Route path="Schedule" element={<Raspisanya />} />
-      </Route>
-    </Routes>
-  );
-}
+//         <Route path="Schedule" element={<Raspisanya />} />
+//       </Route>
+//     </Routes>
+//   );
+// }
 
-export default AppRouter;
+// export default AppRouter;
