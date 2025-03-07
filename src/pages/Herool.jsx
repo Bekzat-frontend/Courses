b437f8130b44ddc1577834ee81b84ee343cd34ce
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-03-08 01:59:40 +0700

    iahdncm

--- src/pages/Herool.jsx
+++ src/pages/Herool.jsx
@@ -1,47 +1,47 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import styled from "styled-components";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-// function Herool() {
-//   return (
-//     <div>
-//       <StyledNav>
-//         <StyledH1>Peaksoft</StyledH1>
-//         <StyledNavBar>
-//           <li>
-//             <Link to="/Courses">Курстар</Link>
-//           </li>
-//           <li>
-//             <Link to="/Schedule">Расписание</Link>
-//           </li>
-//         </StyledNavBar>
-//       </StyledNav>
-//       <div></div>
-//     </div>
-//   );
-// }
-// const StyledNav = styled.nav`
-//   width: 304px;
-//   background-color: #f5f5f5;
-//   height: 100vh;
-//   position: fixed;
-// `;
-// const StyledH1 = styled.h1`
-//   padding-top: 50px;
-//   padding-left: 60px;
-//   font-weight: 900;
-//   font-size: 40px;
-//   color: #006effbe;
-// `;
-// const StyledNavBar = styled.ul`
-//   list-style: none;
-//   padding-top: 80px;
-//   padding-left: 60px;
-//   display: flex;
-//   flex-direction: column;
-//   row-gap: 10px;
-//   font-weight: 800;
-//   font-size: 20px;
-// `;
+function Herool() {
+  return (
+    <div>
+      <StyledNav>
+        <StyledH1>Peaksoft</StyledH1>
+        <StyledNavBar>
+          <li>
+            <Link to="/Courses">Курстар</Link>
+          </li>
+          <li>
+            <Link to="/Schedule">Расписание</Link>
+          </li>
+        </StyledNavBar>
+      </StyledNav>
+      <div></div>
+    </div>
+  );
+}
+const StyledNav = styled.nav`
+  width: 304px;
+  background-color: #f5f5f5;
+  height: 100vh;
+  position: fixed;
+`;
+const StyledH1 = styled.h1`
+  padding-top: 50px;
+  padding-left: 60px;
+  font-weight: 900;
+  font-size: 40px;
+  color: #006effbe;
+`;
+const StyledNavBar = styled.ul`
+  list-style: none;
+  padding-top: 80px;
+  padding-left: 60px;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+  font-weight: 800;
+  font-size: 20px;
+`;
 
-// export default Herool;
+export default Herool;
